59d267066efab6def88051b42c44aa3319421199
Hammad Tauqir <hammadtauqir@gmail.com>
2026-07-16 16:16:08 -0800

    refactor: replace Home component with SignupPage and simplify layout

--- app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignupForm from "@/components/auth/SignupForm";
-
-export default function SignupPage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-100 p-4">
-      <SignupForm />
-    </main>
-  );
-}
