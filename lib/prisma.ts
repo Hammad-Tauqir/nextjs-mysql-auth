b6206aab9d734833adfa9b5f4204249207e53f31
Hammad Tauqir <hammadtauqir@gmail.com>
2026-07-17 10:46:18 +0500

    changes in prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,27 @@
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "@/generated/prisma/client";
+
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+
+const adapter = new PrismaMariaDb({
+  host: "localhost",
+  user: "root",
+  password: process.env.MYSQL_PASSWORD!,
+  database: "nextjs_app",
+  port: 3306,
+  connectionLimit: 5,
+});
+
+
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient();
+  new PrismaClient({
+    adapter,
+  });
 
 
 if (process.env.NODE_ENV !== "production") {
